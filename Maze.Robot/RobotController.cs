b7fba3d5cee4d73957e15c76b334982ccf249b00
Wolfgang <baum.haus123@gmx.at>
2017-10-10 12:45:43 +0200

    Lösung

--- Maze.Robot/RobotController.cs
+++ Maze.Robot/RobotController.cs
@@ -1,20 +1,22 @@
 ﻿using Maze.Library;
+using System.Collections.Generic;
+using System.Drawing;
 
-namespace Maze.Solver
-{
+namespace Maze.Solver {
     /// <summary>
     /// Moves a robot from its current position towards the exit of the maze
     /// </summary>
-    public class RobotController
-    {
+    public class RobotController {
+
         private IRobot robot;
+        private List<Point> robotPoints = new List<Point>();
+        private bool reachedEnd = false;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RobotController"/> class
         /// </summary>
         /// <param name="robot">Robot that is controlled</param>
-        public RobotController(IRobot robot)
-        {
+        public RobotController(IRobot robot) {
             // Store robot for later use
             this.robot = robot;
         }
@@ -29,18 +31,107 @@ namespace Maze.Solver
         /// the exit is not reachable, it has to call <see cref="IRobot.HaltAndCatchFire"/>
         /// and exit.
         /// </remarks>
-        public void MoveRobotToExit()
-        {
+        public void MoveRobotToExit() {
             // Here you have to add your code
 
             // Trivial sample algorithm that can just move right
-            var reachedEnd = false;
+            int x = 0;
+            int y = 0;
+
             robot.ReachedExit += (_, __) => reachedEnd = true;
 
-            while (!reachedEnd)
+            this.checkDirections(x, y);
+
+            if (this.reachedEnd == false) {
+                this.robot.HaltAndCatchFire();
+            }
+
+            // First try
+            /*           while (!reachedEnd)
+                       {
+                           if (robotPoints.Contains(new Point(x, y)) == false)
+                           {
+                               robotPoints.Add(new Point(x, y));
+                               // robot.Move(Direction.Right);
+                               if (this.robot.TryMove(Direction.Right) == true)
+                               {
+                                   if (robotPoints.Contains(new Point(x, y)) == false)
+                                   {
+                                       x = x + 1;
+                                       robotPoints.Add(new Point(x, y));
+                                       this.robot.Move(Direction.Right);
+                                   }
+                               }
+                               if (this.robot.TryMove(Direction.Left) == true && reachedEnd == false)
+                               {
+                                   if (robotPoints.Contains(new Point(x, y)) == false)
+                                   {
+                                       x = x - 1;
+                                       robotPoints.Add(new Point(x, y));
+                                       this.robot.Move(Direction.Left);
+                                   }
+                               }
+                               if (this.robot.TryMove(Direction.Up) == true && reachedEnd == false)
+                               {
+                                   if (robotPoints.Contains(new Point(x, y)) == false)
+                                   {
+                                       y = y - 1;
+                                       robotPoints.Add(new Point(x, y));
+                                       this.robot.Move(Direction.Up);
+                                   }
+                               }
+                               if (this.robot.TryMove(Direction.Down) == true && reachedEnd == false)
+                               {
+                                   if (robotPoints.Contains(new Point(x, y)) == false)
+                                   {
+                                       y = y + 1;
+                                       robotPoints.Add(new Point(x, y));
+                                       this.robot.Move(Direction.Down);
+                                   }
+                               }
+                           }
+                       } */
+
+        }
+
+        public void checkDirections(int x, int y) {
+            if (this.robotPoints.Contains(new Point(x, y)) == false && this.reachedEnd == false)
             {
-                robot.Move(Direction.Right);
+                this.robotPoints.Add(new Point(x, y));
+                if (this.reachedEnd == false && this.robot.TryMove(Direction.Right) == true)
+                {
+                    this.checkDirections(x + 1, y);
+                    if (this.reachedEnd == false)
+                    {
+                        this.robot.Move(Direction.Left);
+                    }
+                }
+                if (this.reachedEnd == false && this.robot.TryMove(Direction.Left) == true)
+                {
+                    this.checkDirections(x - 1, y);
+                    if (this.reachedEnd == false)
+                    {
+                        this.robot.Move(Direction.Right);
+                    }
+                }
+                if (this.reachedEnd == false && robot.TryMove(Direction.Up) == true)
+                {
+                    this.checkDirections(x, y - 1);
+                    if (this.reachedEnd == false)
+                    {
+                        this.robot.Move(Direction.Down);
+                    }
+                }
+                if (this.reachedEnd == false && robot.TryMove(Direction.Down) == true)
+                {
+                    this.checkDirections(x, y + 1);
+                    if (this.reachedEnd == false)
+                    {
+                        this.robot.Move(Direction.Up);
+                    }
+                }
             }
         }
+
     }
 }
